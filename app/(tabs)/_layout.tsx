9c223173e97c680ef1f39d624a9e1c02b6b44631
ahmetpalavan <ahmetpalavann@gmail.com>
2023-10-30 17:43:55 +0300

    Refactor TabLayout and TabOneScreen components
    - Updated import statements to use double quotes instead of single quotes
    - Added import statement for the useCartStore hook
    - Added a new constant, items, which is destructured from the useCartStore hook
    - Updated the screenOptions prop in the Tabs component to include headerStyle and headerTintColor properties
    - Replaced the Pressable component with TouchableOpacity in the headerRight prop of the first Tabs.Screen component
    - Wrapped the FontAwesome icon inside a Text component and added styling to display the number of items in cart
    - Removed unused imports
    
    Refactor TabOneScreen and TabTwoScreen components
    
    - Updated import statements to use double quotes instead of single quotes
    - Added import statement for the useCartStore hook
    - Added a new constant, addToCart, removeFromCart, and cart, which are destructured from the useCartStore hook
    - Created a renderItem function that renders each item in a FlatList with an image, title, price, and buttons to add or remove items from cart

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,49 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, Text, TouchableOpacity, useColorScheme } from "react-native";
 
-import Colors from '../../constants/Colors';
+import Colors from "../../constants/Colors";
+import useCartStore from "@/store/cartStore";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>["name"]; color: string }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { items } = useCartStore();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerStyle: {
+          backgroundColor: "#1C3782"
+        },
+        headerTintColor: "#fff",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: "Tab One",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
+              <TouchableOpacity style={{ marginRight: 10 }}>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    color: "#fff",
+                  }}
+                >
+                  {items}
+                </Text>
+              </TouchableOpacity>
             </Link>
           ),
         }}
@@ -46,7 +51,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: "Tab Two",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, Image, ListRenderItem, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { View } from "../../components/Themed";
+import data from "@/assets/data.json";
+import { Ionicons } from "@expo/vector-icons";
+import useCartStore from "@/store/cartStore";
+import { Product } from "@/store/interfaces";
 
 export default function TabOneScreen() {
+  const { addToCart, removeFromCart, items } = useCartStore();
+  const renderItem: ListRenderItem<Product & { quantity?: number }> = ({ item }) => {
+    return (
+      <View style={styles.cartContainer}>
+        <Image source={{ uri: item.image }} style={styles.cartImage} />
+        <View style={styles.itemContainer}>
+          <Text style={styles.titleContainer}>{item.title}</Text>
+          <Text>$ {item.price}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              addToCart(item);
+            }}
+            style={{ padding: 10 }}
+          >
+            <Ionicons name="add-circle-outline" size={24} color="black" />
+          </TouchableOpacity>
+          <Text style={{ padding: 10 }}>{item.quantity ?? 0}</Text>
+          <TouchableOpacity
+            onPress={() => {
+              removeFromCart(item);
+            }}
+            style={{ padding: 10 }}
+          >
+            <Ionicons name="remove-circle-outline" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <FlatList data={data} renderItem={renderItem} />
     </View>
   );
 }
@@ -16,16 +49,36 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  cartContainer: {
+    marginBottom: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 20,
+  },
+  cartImage: {
+    width: 50,
+    height: 50,
+  },
+  itemContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  titleContainer: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
