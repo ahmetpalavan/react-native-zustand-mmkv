4642401d0fb9df1f84d24791bb4737deca26f174
ahmetpalavan <ahmetpalavann@gmail.com>
2023-10-30 17:44:14 +0300

    feat: Add cartStore and interfaces
    - Added a new file `cartStore.ts` which contains the implementation of the cart store.
    - Added a new file `interfaces.ts` which defines the `Product` interface.
    - The `cartStore.ts` file includes functions to add, remove, and clear items from the cart.
    - The `addToCart` function increments the quantity of an existing item or adds a new item to the cart.
    - The `removeFromCart` function decrements the quantity of an item in the cart or removes it if its quantity becomes zero.
    - The `clearCart` function empties the cart and resets the number of items to zero.

---
+++ store/cartStore.ts
@@ -0,0 +1,52 @@
+import { create } from "zustand";
+import { Product } from "@/store/interfaces";
+
+type CartStore = {
+  cart: Array<Product & { quantity: number }>;
+  addToCart: (product: Product) => void;
+  removeFromCart: (product: Product) => void;
+  clearCart: () => void;
+  items: number;
+};
+
+const useCartStore = create<CartStore>((set) => ({
+  cart: [],
+  items: 0,
+  addToCart: (product: Product) =>
+    set((state) => {
+      state.items++;
+      const hasItem = state.cart.find((item) => item.id === product.id);
+      if (!hasItem) {
+        return {
+          cart: [...state.cart, { ...product, quantity: 1 }],
+        };
+      } else {
+        const cart = [...state.cart];
+        const index = cart.findIndex((item) => item.id === product.id);
+        cart[index].quantity += 1;
+        return {
+          cart,
+        };
+      }
+    }),
+  removeFromCart: (product: Product) =>
+    set((state) => {
+      return {
+        cart: state.cart
+          .map((item) => {
+            if (item.id === product.id) {
+              state.items--;
+              return {
+                ...item,
+                quantity: item.quantity - 1,
+              };
+            }
+            return item;
+          })
+          .filter((item) => item.quantity > 0),
+      };
+    }),
+  clearCart: () => set(() => ({ cart: [], items: 0 })),
+}));
+
+export default useCartStore;
---
+++ store/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
